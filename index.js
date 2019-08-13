a32b3f0d50517884225b950a22d829fdb3ca1fca
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-08-13 12:52:14 +0700

    factory function & constructor function

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 // OBJECT LITERALS
 
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1
-  },
-  draw: function () {
+// const circle = {
+//   radius: 1,
+//   location: {
+//     x: 1,
+//     y: 1
+//   },
+//   draw: function () {
+//     console.log('draw');
+//   }
+// };
+
+// circle.draw();
+
+
+// FACTORIES
+// const circle = {
+//   radius: 1,
+//   location: {
+//     x: 1,
+//     y: 1
+//   }
+// };
+
+// const circle = {
+//   radius: 2,
+//   location: {
+//     x: 2,
+//     y: 2
+//   }
+// };
+// circle.draw();
+
+// DIRINGKAS menggunakan factories / constructor:
+// Factory Function:
+
+ function createCircle(radius) {
+   return {
+     radius,
+     draw: function() {
+       console.log('draw');
+     }
+   };
+ }
+ const circle = createCircle(1);
+ circle.draw();
+
+//  Constructor Function:
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
     console.log('draw');
   }
-};
-
-circle.draw();
+}
+const another = new Circle(1);
